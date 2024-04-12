90092acb28e10e52844e940770d4ffc8c45e0bd3
Viktor Podzigun <viktor.podzigun@gmail.com>
2024-04-12 23:25:08 +0200

    Added FileList actions definitions

--- src/FileListActions.d.ts
+++ src/FileListActions.d.ts
@@ -1,4 +1,13 @@
-export type FileListAction = FileListAction.FileListParamsChangedAction;
+import { FileListDir } from "./api/FileListDir";
+import { SortMode } from "./sort/SortMode.mjs";
+
+export type FileListAction =
+  | FileListAction.FileListParamsChangedAction
+  | FileListAction.FileListDirChangedAction
+  | FileListAction.FileListDirUpdatedAction
+  | FileListAction.FileListItemCreatedAction
+  | FileListAction.FileListDiskSpaceUpdatedAction
+  | FileListAction.FileListSortAction;
 
 export namespace FileListAction {
   interface FileListParamsChangedAction {
@@ -6,5 +15,32 @@ export namespace FileListAction {
     readonly offset: number;
     readonly index: number;
     readonly selectedNames: Set<string>;
+  }
+
+  interface FileListDirChangedAction {
+    readonly action: "FileListDirChangedAction";
+    readonly dir: string;
+    readonly currDir: FileListDir;
+  }
+
+  interface FileListDirUpdatedAction {
+    readonly action: "FileListDirUpdatedAction";
+    readonly currDir: FileListDir;
+  }
+
+  interface FileListItemCreatedAction {
+    readonly action: "FileListItemCreatedAction";
+    readonly name: string;
+    readonly currDir: FileListDir;
+  }
+
+  interface FileListDiskSpaceUpdatedAction {
+    readonly action: "FileListDiskSpaceUpdatedAction";
+    readonly diskSpace: number;
+  }
+
+  interface FileListSortAction {
+    readonly action: "FileListSortAction";
+    readonly mode: SortMode;
   }
 }
